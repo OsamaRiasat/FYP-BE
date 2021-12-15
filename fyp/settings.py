94340fc65875cb04f4086a8a8552bf7c0a949159
OsamaRiasat <osamariasat@gmail.com>
2021-12-15 13:56:51 +0500

    ok

--- fyp/settings.py
+++ fyp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0byeum1k9phqh^^cs7xg_b@o2e^wv#g5gxos&w!-kakbx74_t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
