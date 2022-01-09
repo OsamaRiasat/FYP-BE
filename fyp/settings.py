6ad8b36701fcfa4f79024a594acc503e94b6f7df
OsamaRiasat <osamariasat@gmail.com>
2022-01-09 14:43:00 +0500

    CORS

--- fyp/settings.py
+++ fyp/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
